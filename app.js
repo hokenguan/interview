fbcd5106203291491e614556a4a6ddd3b41c58f5
Hoken  Guan <h2020856@hotmail.com>
2018-07-15 16:45:35 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname+'/bower_components'));
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.get('/', function(req,res){  
+  res.render("b.ejs",{
+           names:["cd","lw"]      //第一个参数是给ejs渲染的内容
+           //filename:__dirname     //第二个参数是设置include路径的 不写就找不到 报错
+       })
+      }
+)
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
